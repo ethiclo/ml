0ba1a1a585318b3f8dbdc1f60519751a2895eb78
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2023-03-18 16:33:15 -0400

    added server

--- server.py
+++ server.py
@@ -1,5 +1,13 @@
+"""
+Server for ethiclo
+"""
+
 import os
 from flask import Flask, request, jsonify
+import psycopg2 as pg
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Initialize flask app
 server = Flask(__name__)
@@ -14,12 +22,40 @@ def healthcheck():
 
 
 @server.route('/send_url/<string:url>/<string:user>', methods=["POST"])
-def url(url: str, user: str) -> None:
+def add_url(url: str, user: str) -> None:
     """Send a URL to the db.
     """
-    pass
 
-#  TODO: Make a route to classify, query, send back info to the frontend
+    # Connect to the db
+    conn = connect()
+    cur = conn.cursor()
+
+
+@server.route('/get_url/<string:user>', methods=["GET"])
+def get_url(user: str) -> None:
+    """Get a url for user from the database
+    """
+
+
+@server.route('/get_sustainable_products/<string:url>/<string:user>',
+              methods=["GET"])
+def get_sustainable_products(url: str, user: str) -> None:
+    """Fetch sustainable alternatives from the database
+    """
+
+
+def connect():
+    """Connect to the db.
+    """
+    try:
+        connection = pg.connect(
+            dbname=os.environ["PGDATABASE"], user=os.environ["PGUSER"],
+            password=os.environ["PASSWORD"],
+            options="-c search_path=ethiclo"
+        )
+        return connection
+    except pg.Error:
+        return None
 
 
 if __name__ == '__main__':
@@ -29,3 +65,4 @@ if __name__ == '__main__':
     # app.run(host="0.0.0.0", port=8080, threaded=True)
     server.run(host="0.0.0.0", port=int(
         os.environ.get("PORT", 4000)), threaded=True)
+    # print(os.environ)
